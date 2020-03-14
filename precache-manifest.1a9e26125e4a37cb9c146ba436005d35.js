cc1b5ccb8f461c79715171c2876874b2deb6293a
Li-Hsuan <lihsuanlung@gmail.com>
2020-03-14 17:04:06 -0400

    Updates

---
+++ precache-manifest.1a9e26125e4a37cb9c146ba436005d35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32da9c42f913d6a37d84a481c5311534",
+    "url": "/coronavirus-epidemiology-model-chart/index.html"
+  },
+  {
+    "revision": "15916afe936b71692d2e",
+    "url": "/coronavirus-epidemiology-model-chart/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6cfbbbad6c40ec7fcdda",
+    "url": "/coronavirus-epidemiology-model-chart/static/js/2.a70b3c9b.chunk.js"
+  },
+  {
+    "revision": "15916afe936b71692d2e",
+    "url": "/coronavirus-epidemiology-model-chart/static/js/main.a23f397a.chunk.js"
+  },
+  {
+    "revision": "006a294257fa73b7a4b1",
+    "url": "/coronavirus-epidemiology-model-chart/static/js/runtime-main.db7d73f6.js"
+  }
+]);
